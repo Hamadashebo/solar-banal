5e7ee8a76fe0b89dd51ae6e72f8edf42dcc7a1cd
Ahmed Shabana <167260717+Hamadashebo@users.noreply.github.com>
2024-10-01 14:06:08 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function dropdown(){
+    document.querySelector("#dropdown").style.display="none" 
+    document.querySelector("ul").style.display="block"
+    document.querySelector("ul").style.gap="5px"
+    document.querySelector("ul").style.position="absolute"
+}
